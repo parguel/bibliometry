c0e8ee0815d0f24bfbdc97fb9d3d388e2e56485d
JanPerret <jan.perret@gmail.com>
2019-12-06 13:19:58 +0100

    added search function

--- R/functions.R
+++ R/functions.R
@@ -25,29 +25,34 @@ search_taxa <- function() {
   search_df <- corpus_df %>% 
     select(UT, TI, AB, DE) %>% 
     remove_rownames() %>% 
-    summarize(search_mix = paste0(TI, AB, DE,collapse = " ")) %>% 
+    unite("search_mix", TI:DE) %>% 
     add_column(animal = NA, plant = NA)
   
-  
   # replacing special caracters and removing multiple spaces
-  search_df <- search_df %>% 
-    str_replace_all(".", " ")
-    
-  data <- '"I am a, new comer","to r,"please help","me:out","here"'
-  gsub('[[:punct:] ]+',' ',data)
-  # [1] " I am a new comer to r please help me out here "
+  search_df$search_mix <- gsub('[[:punct:] ]+',' ', search_df$search_mix)
   
   # list of the words to search 
-  animal_words = c(" animal "," mammal "," mammals "," bird "," birds "," amphibian "," amphibians "," reptile "," reptiles "," insect "," insects ")
+  animal_words = c(" animal "," mammal "," mammals "," bird "," birds "," amphibian "," amphibians "," reptile "," reptiles "," insect "," insects ", " crab "," crabs ")
   plant_words = c(" plant "," plants "," gymnosperm "," gymnosperms "," angiosperm "," angiosperms ")
   
   # searching keywords in titles, abstracts and author keywords
   for (i in 1:length(search_df$UT)) {
     article_search_mix = search_df$search_mix[i]
     
-    grepl("toyota", 'TOYOTA subaru', ignore.case = TRUE)
-
+    # search of the animal keywords
+    for (n in 1:length(animal_words)){
+      if (grepl(animal_words[n], article_search_mix, ignore.case = TRUE)) {
+        search_df$animal[i] <- "yes"
+      }
+    }
     
+    # search of the plant keywords
+    for (n in 1:length(plant_words)){
+      if (grepl(plant_words, article_search_mix, ignore.case = TRUE)) {
+        search_df$plant[i] <- "yes"
+      }
+    }
   }
-    
- }
+  
+  
+}
